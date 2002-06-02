8618bf23ce50c2c178c7b5589e772a71ee70bf8c
Christian Dickmann <dickmann@php.net>
2002-06-02 12:57:19 +0000

    add some instructions on how to install the package
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_Frontend_Web/trunk@84422 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ docs/example.php
@@ -0,0 +1,31 @@
+<?php
+    /**
+     * Installation of PEAR_Frontend_Web:
+     *
+     * 'pear install PEAR_Frontend_Web'
+     * Create a __secure__ directory accessable by your webserver
+     * put a file like this one in there.
+     * Create a directory for PEAR to be installed in and add it to 
+     * the include path. (Yes. you can use your standard PEAR dir,
+     * but the Webserver needs writing access, so I think for this
+     * beta software a new directory is more safe)
+     * Specify a file for your PEAR config.
+     * Have fun ...
+     *
+     * by Christian Dickmann <dickmann@php.net>
+     */
+
+
+    // Rebuild Includepath (optional)
+    $include_path = array(
+        '.',
+        dirname(__FILE__).'/pear',
+        );
+    ini_set('include_path', implode(':', $include_path);
+
+    // Configfile
+    $pear_user_config    = dirname(__FILE__)."/pear.conf";
+
+    // Include WebInstaller
+    require_once("PEAR/WebInstaller.php");
+?>
