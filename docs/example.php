5a9f3a02c492376e94de98f2d378ef496538c6c1
Pierre-Alain Joye <pajoye@php.net>
2003-05-27 23:42:04 +0000

    - Clean up - Some little fixes while looking for the 'wb' bug ;) - Use the ENV variables as in other frontend - Use the already powerfull package tools ;)
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_Frontend_Web/trunk@128953 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/example.php
+++ docs/example.php
@@ -1,38 +1,91 @@
 <?php
-    /**
-     * Installation of PEAR_Frontend_Web:
-     *
-     * 'pear install PEAR_Frontend_Web'
-     * Create a __secure__ directory accessable by your webserver
-     * put a file like this one in there.
-     * Create a directory for PEAR to be installed in and add it to 
-     * the include path. (Yes. you can use your standard PEAR dir,
-     * but the Webserver needs writing access, so I think for this
-     * beta software a new directory is more safe)
-     * Specify a file for your PEAR config.
-     * Have fun ...
-     *
-     * by Christian Dickmann <dickmann@php.net>
-     */
+/**
+ * Installation of PEAR_Frontend_Web:
+ *
+ * 'pear install PEAR_Frontend_Web'
+ * Create a __secure__ directory accessable by your webserver
+ * put a file like this one in there.
+ * Create a directory for PEAR to be installed in and add it to
+ * the include path. (Yes. you can use your standard PEAR dir,
+ * but the Webserver needs writing access, so I think for this
+ * beta software a new directory is more safe)
+ * Specify a file for your PEAR config.
+ * Have fun ...
+ *
+ * by Christian Dickmann <dickmann@php.net>
+ */
+/*
+if ($env=getenv('PHP_PEAR_SYSCONF_DIR')) {
+    define("PHP_PEAR_SYSCONF_DIR",$env);
+} else {
+    putenv("PHP_PEAR_SYSCONF_DIR=d:/www/test1");
+}
+if ($env=getenv('PHP_PEAR_HTTP_PROXY')) {
+    define("PHP_PEAR_HTTP_PROXY",$env);
+} else {
+    putenv("PHP_PEAR_HTTP_PROXY=");
+}
+*/
+if ($env=getenv('PHP_PEAR_INSTALL_DIR')) {
+    define("PHP_PEAR_INSTALL_DIR",$env);
+} else {
+    putenv('PHP_PEAR_INSTALL_DIR=@php_dir@');
+}
+/*
+if ($env=getenv('PHP_PEAR_EXTENSION_DIR')) {
+    define("PHP_PEAR_EXTENSION_DIR",$env);
+} else {
+    putenv("PHP_PEAR_EXTENSION_DIR=d:/www/test1/pear/PEAR/extensions");
+}
+*/
+/*
+if ($env=getenv('PHP_PEAR_DOC_DIR')) {
+    define("PHP_PEAR_DOC_DIR",$env);
+} else {
+    putenv("PHP_PEAR_DOC_DIR=d:/www/test1/pear/PEAR/docs");
+}
+*/
+if ($env=getenv('PHP_PEAR_BIN_DIR')) {
+    define("PHP_PEAR_BIN_DIR",$env);
+} else {
+    putenv('PHP_PEAR_BIN_DIR=@bin_dir@');
+}
+/*
+if ($env=getenv('PHP_PEAR_DATA_DIR')) {
+    define("PHP_PEAR_DATA_DIR",$env);
+} else {
+    putenv("PHP_PEAR_DATA_DIR=d:/www/test1/pear/PEAR/data");
+}
 
-    require_once(dirname(__FILE__).'/PEAR/PEAR.php');
-    if (OS_WINDOWS) {
-        $seperator = ';';
-    } else {
-        $seperator = ':';
-    };
-    
-    // Rebuild Includepath (optional)
-    $include_path = array(
-//        '.', // disabled to avoid conflict with PEAR::Config
-        dirname(__FILE__).'/PEAR',
-        );
-    ini_set('include_path', implode($seperator, $include_path));
+if ($env=getenv('PHP_PEAR_TEST_DIR')) {
+    define("PHP_PEAR_TEST_DIR",$env);
+} else {
+    putenv("PHP_PEAR_TEST_DIR=d:/www/test1/pear/PEAR/tests");
+}
+if ($env=getenv('PHP_PEAR_CACHE_DIR')) {
+    define("PHP_PEAR_CACHE_DIR",$env);
+} else {
+    putenv("PHP_PEAR_CACHE_DIR=d:/www/test1/cache");
+}
+*/
+if ($env=getenv('PHP_PEAR_PHP_BIN')) {
+    define("PHP_PEAR_PHP_BIN",$env);
+} else {
+    putenv('PHP_PEAR_PHP_BIN=@php_bin@');
+}
+$env=getenv('PHP_PEAR_INSTALL_DIR');
+require_once($env.'/PEAR.php');
+if (OS_WINDOWS) {
+    $seperator = ';';
+} else {
+    $seperator = ':';
+};
 
-    // Configfile
-    $pear_user_config = dirname(__FILE__)."/pear.conf";
-    $useDHTML         = true;
-    
-    // Include WebInstaller
-    require_once("PEAR/WebInstaller.php");
+// Rebuild Includepath (optional)
+ini_set('include_path', '@include_path@');
+// Configfile
+//$pear_user_config = dirname(__FILE__)."/pear.conf";
+$useDHTML         = true;
+// Include WebInstaller
+require_once("PEAR/WebInstaller.php");
 ?>
