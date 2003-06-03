0d201f2ff4a85b1292c31b1056a204dae2b5ff49
Pierre-Alain Joye <pajoye@php.net>
2003-06-03 23:41:22 +0000

    remove commented codes
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_Frontend_Web/trunk@130020 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/example.php
+++ docs/example.php
@@ -14,60 +14,17 @@
  *
  * by Christian Dickmann <dickmann@php.net>
  */
-/*
-if ($env=getenv('PHP_PEAR_SYSCONF_DIR')) {
-    define("PHP_PEAR_SYSCONF_DIR",$env);
-} else {
-    putenv("PHP_PEAR_SYSCONF_DIR=d:/www/test1");
-}
-if ($env=getenv('PHP_PEAR_HTTP_PROXY')) {
-    define("PHP_PEAR_HTTP_PROXY",$env);
-} else {
-    putenv("PHP_PEAR_HTTP_PROXY=");
-}
-*/
 if ($env=getenv('PHP_PEAR_INSTALL_DIR')) {
     define("PHP_PEAR_INSTALL_DIR",$env);
 } else {
     putenv('PHP_PEAR_INSTALL_DIR=@php_dir@');
 }
-/*
-if ($env=getenv('PHP_PEAR_EXTENSION_DIR')) {
-    define("PHP_PEAR_EXTENSION_DIR",$env);
-} else {
-    putenv("PHP_PEAR_EXTENSION_DIR=d:/www/test1/pear/PEAR/extensions");
-}
-*/
-/*
-if ($env=getenv('PHP_PEAR_DOC_DIR')) {
-    define("PHP_PEAR_DOC_DIR",$env);
-} else {
-    putenv("PHP_PEAR_DOC_DIR=d:/www/test1/pear/PEAR/docs");
-}
-*/
+
 if ($env=getenv('PHP_PEAR_BIN_DIR')) {
     define("PHP_PEAR_BIN_DIR",$env);
 } else {
     putenv('PHP_PEAR_BIN_DIR=@bin_dir@');
 }
-/*
-if ($env=getenv('PHP_PEAR_DATA_DIR')) {
-    define("PHP_PEAR_DATA_DIR",$env);
-} else {
-    putenv("PHP_PEAR_DATA_DIR=d:/www/test1/pear/PEAR/data");
-}
-
-if ($env=getenv('PHP_PEAR_TEST_DIR')) {
-    define("PHP_PEAR_TEST_DIR",$env);
-} else {
-    putenv("PHP_PEAR_TEST_DIR=d:/www/test1/pear/PEAR/tests");
-}
-if ($env=getenv('PHP_PEAR_CACHE_DIR')) {
-    define("PHP_PEAR_CACHE_DIR",$env);
-} else {
-    putenv("PHP_PEAR_CACHE_DIR=d:/www/test1/cache");
-}
-*/
 if ($env=getenv('PHP_PEAR_PHP_BIN')) {
     define("PHP_PEAR_PHP_BIN",$env);
 } else {
@@ -81,10 +38,7 @@ if (OS_WINDOWS) {
     $seperator = ':';
 };
 
-// Rebuild Includepath (optional)
 ini_set('include_path', '@include_path@');
-// Configfile
-//$pear_user_config = dirname(__FILE__)."/pear.conf";
 $useDHTML         = true;
 // Include WebInstaller
 require_once("PEAR/WebInstaller.php");
